e4fc95b4def51f5ccdb8ef59c1dda1f8b83a5eb9
EgeSaykan <shk.ync2005@gmail.com>
2025-10-21 23:20:05 +0100

    Two Sum

--- 00001_two_sum.cpp
+++ 00001_two_sum.cpp
@@ -1,33 +1,21 @@
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 
+using namespace std;
 
 class Solution {
 public:
 
-    size_t max_vec(std::vector<int>& nums) {
-        size_t biggest = nums[0];
-        for (size_t i = 0; i < nums.size(); i++) {
-            if (nums[i] > biggest) biggest = nums[i];
-        }
-        return biggest;
-    }
+    vector<int> twoSum(std::vector<int>& nums, int target) {
+        unordered_map <int, int> u_map;
 
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        
-        size_t biggest = max_vec(nums);
-        biggest = (biggest < static_cast<size_t>(target))  ? biggest : static_cast<size_t>(target); 
-        std::vector<int> array(++biggest, -1);
-        for (size_t i = 0; i < nums.size(); i++) {
-            if (nums[i] <= target) {
-                array[nums[i]] = (int)i;
+        for (int i = 0; i < nums.size(); i++) {
+            if (u_map.count(target - nums[i]) >= 1) {
+                return {u_map[target - nums[i]], i};
             }
-        }
-        for (size_t i = 0; i < nums.size(); i++) {
-            if (nums[i] <= target) {
-                if (array[target - nums[i]] != i && array[target - nums[i]] != -1) {
-                    return {(int)i, array[target - nums[i]]};
-                }
+            else {
+                u_map[nums[i]] = i;
             }
         }
         return {};
@@ -36,8 +24,8 @@ public:
 
 int main() {
     Solution solution;
-    std::vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
+    std::vector<int> nums = {5, -75, -25};
+    int target = -100;
     std::vector<int> result = solution.twoSum(nums, target);
     std::cout << result[0] << "  " << result[1];
     return 0;
