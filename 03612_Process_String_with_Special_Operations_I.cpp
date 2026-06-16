a349c50eb97eee0db1520a9414f7a5c9462a724c
EgeSaykan <shk.ync2005@gmail.com>
2026-06-16 16:00:32 +0100

    03612

---
+++ 03612_Process_String_with_Special_Operations_I.cpp
@@ -0,0 +1,50 @@
+#include<string>
+#include<iostream>
+#include <algorithm> 
+
+using namespace std;
+
+class Solution {
+public:
+
+    void remove(string& s) {
+        if (s.size() > 0) {s.pop_back();}
+    }
+
+    void add(string& s, char& c) {
+        s += c;
+    }
+
+    void duplicate(string& s) {
+        s += s;
+    }
+
+
+    string processStr(string s) {
+        string result;
+        while (s.size() > 0) {
+            char c = s[0];
+            s.erase(0, 1);
+
+            if (96 < c && c < 123) {add(result, c);}
+            else if (c == '*') {remove(result);}
+            else if (c == '#') {duplicate(result);}
+            else if (c == '%') {reverse(result.begin(), result.end());}
+
+        }
+        return result;
+    }
+};
+
+
+
+int main() {
+    Solution solution;
+
+    string a = "hel*lo%kitty";
+    string result = solution.processStr(a);
+    cout << result << endl;
+    cout << endl;
+
+    return 0;
+}
